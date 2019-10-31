f3155c4177d5cd4de6311ee6720e09feab6eba14
vikashvishnu1508 <vikashvishnu1508@gmail.com>
2019-11-01 01:15:24 +0530

    display pizza count

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import PizzaNames
 # Create your views here.
 def index(request):
-    return render(request, 'orders/index.html')
+    context = {
+        "pizzas" : PizzaNames.objects.all(),
+        "pizzasCount" : PizzaNames.objects.all().count()
+    }
+    return render(request, 'orders/index.html', context)
     #return HttpResponse("Project 3: TODO")
